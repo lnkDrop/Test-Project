695fb2aa53edd6fb77e41ca8337aa0ed7bd3ea7b
INk <18487331461@163.com>
2019-05-12 14:03:41 +0800

    真题匹配

--- wordtest.py
+++ wordtest.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-
+# 获取四六级真题的单词，和词频的统计（字典方式）
 import docx
 import re
 
-f = docx.Document(r"D:/Project/真题数据源/2013061.docx") #读取四六级真题
-dict = {}
-for para in f.paragraphs:
-    txt = para.text
-    result_list = re.findall(r'[a-zA-Z]+', txt)   #正则表达式提取出英文单词
-    for key in result_list:
-        dict[key] = dict.get(key, 0) + 1        #把出现的单词放入字典，按出现次数进行词频统计
-print(dict)
 
-# x = ['a','a','b','c','c','c']
-# for key in x:
-#     dict[key] = dict.get(key, 0)+1
-# print(dict)
+def getword():
+    dict = {}
+    for i in range(2):
+        cd = r"D:/Project/真题数据源/" + str(i + 1) + ".docx"
+        f = docx.Document(cd)  # 读取四六级真题
+        for para in f.paragraphs:
+            txt = para.text
+            result_list = re.findall(r'[a-zA-Z]+', txt)  # 正则表达式提取出英文单词
+            for key in result_list:
+                dict[key] = dict.get(key, 0) + 1  # 把出现的单词放入字典，按出现次数进行词频统计
+    print(dict)  # 查看词频
+    print(len(dict))  # 查看单词总数
+
+
+getword()
---
+++ 真题数据源/test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#@Name: 杨钦凯
+#@Student ID:20161152105
+#@File  : test.py
+for i in range(10):
+    print("D:/Project/真题数据源/"+str(i+1)+".docx")
